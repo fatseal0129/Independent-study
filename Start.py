22d3e16d2ff94333ed44a1417bc7a2bd2893ec2f
YenChenLee <b10909037@mail.ntust.edu.tw>
2023-05-30 02:05:18 +0800

    initial update

--- Start.py
+++ Start.py
@@ -1,5 +1,8 @@
-import uvicorn
+# import uvicorn
+#
+# if __name__ == "__main__":
+#     uvicorn.run("Server.Api.api_Server:app", reload=True)
+#     uvicorn.run("Box.Api.api_frontend:app", reload=True)
+from Server.Service import DB
 
-if __name__ == "__main__":
-    uvicorn.run("Server.Api.api_Server:app", reload=True)
-    uvicorn.run("Box.Api.api_frontend:app", reload=True)
+states = DB.getAllCamName_with_State()
